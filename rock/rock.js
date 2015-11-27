84faeb889d5317a51a92130c749d380d1007f4f9
892280082 <892280082@qq.com>
2015-11-27 17:23:48 +0800

    2

--- rock/rock.js
+++ rock/rock.js
@@ -5,8 +5,38 @@ define(function(require) {
 	var EventMng = require('game/EventMng');
 	var Drawable = require('display/Drawable');
 	var utils = require('util/Utils');
+
+	var canvas = document.getElementById('mainCanvas');
+	var ctx = new CanvasContext({canvas: canvas});
+
+	var hand = new DisplayObject({ name:'hand',width:200,height:200 });
+	hand.setDrawable(document.getElementById('hand'));
+
+	hand.scaleX = 0.5;
+	hand.scaleY = 0.5;
+	hand.x = 100;
+	hand.y = 100;
+	hand.doRota = function(){
+		var setFlag =  setInterval(function(){
+			hand.rotation += 5;
+		},30);
+	}
+	hand.on('click',function(e){
+		this.doRota();
+	});
+
+	var stage = new Stage();
+	var eventMng = new EventMng(canvas, stage);
+	eventMng.init();
+	stage.addChild(hand);
+
 	
 
-
+	setInterval(function() {
+		ctx.clear(0, 0, 800, 600);
+		eventMng.dispatch();
+		hand._update();
+		hand._render(ctx);
+	},30);
 
 });
