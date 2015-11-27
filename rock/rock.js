a01fee2aab3850c03ac7bc859fddcddfc00ec82a
892280082 <892280082@qq.com>
2015-11-27 16:08:38 +0800

    init file

---
+++ rock/rock.js
@@ -0,0 +1,12 @@
+define(function(require) {
+	var Stage = require('display/Stage');
+	var DisplayObject = require('display/DisplayObject');
+	var CanvasContext = require('context/CanvasContext');
+	var EventMng = require('game/EventMng');
+	var Drawable = require('display/Drawable');
+	var utils = require('util/Utils');
+	
+
+
+
+});
